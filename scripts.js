e81b1fc83e6de958b6ac974865b61dd0e20afccf
reillydavis <reillydavis@gmail.com>
2014-09-06 18:55:22 -0700

    adding sidebar and fontawesome

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 function start(sidebar) {
-  $('body').prepend(sidebar);
+	var fa = document.createElement('style');
+	fa.type = 'text/css';
+	fa.textContent = '@font-face { font-family: FontAwesome; src: url("'
+		+ chrome.extension.getURL('bower_components/font-awesome/fonts/fontawesome-webfont.woff')
+		+ '"); }';
+	document.head.appendChild(fa);
+	$('body').prepend(sidebar);
+	$('.cb-sidebar-close').click(function(){
+		$('.cb-sidebar').remove();
+	});
 }
